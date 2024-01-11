5d925f9faa8b7e36bdc24d5ac685882778e93f0e
Nayabi <nayabisafiya1996@gmail.com>
2024-01-11 22:01:58 +0530

    bouncing ball - done

--- index.js
+++ index.js
@@ -4,22 +4,35 @@ const ctx= canvas.getContext("2d");  /* Create a 2d rendering tool that use to d
 /** Creating Function draw will call upon repeatedly with setInterval for showing the movemnts of ball */
 let x = canvas.width / 2;
 let y = canvas.height - 30;
-
 let dx=2;
 let dy=-2;
 
+const ballRadius=10;
+
 function drawBall(){
+   
     ctx.beginPath();
 
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle= "#0095DD";
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle="#0095DD";
     ctx.fill();
     ctx.closePath();
 }
-function draw(){
-ctx.clearRect(0,0,canvas.width,canvas.height);  
-drawBall();
-x+=dx;
-y+=dy;
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    // checking the collision with the wall . if ball collid with wall we change the diection
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+      
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+      
+    }
+  
+    x += dx;
+    y += dy;
+   
 }
 setInterval(draw,10);
