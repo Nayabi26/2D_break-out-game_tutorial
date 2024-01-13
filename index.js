d0823d2eb80481d77b024e33851ca8f09943d31f
Nayabi <nayabisafiya1996@gmail.com>
2024-01-13 13:04:36 +0530

    Added Paddlemoving Logic

--- index.js
+++ index.js
@@ -7,7 +7,44 @@ let y = canvas.height - 30;
 let dx=2;
 let dy=-2;
 
+//Defining paddle height width and starting point
+const paddleHeight=10;
+const paddleWidth=75;
+let paddleX= (canvas.width-paddleWidth)/2
+
 const ballRadius=10;
+
+//Paddle controls variables
+let rightPressed=false;
+let leftPressed= false;
+
+//Addig EventListener to the document
+document.addEventListener("keydown",keyDownHandler,false);
+document.addEventListener("keyup",keyUpHandler,false);
+
+//Defining each event 
+function keyDownHandler(e)
+{
+   if(e.key==="Right" || e.key==="ArrowRight")
+   {
+    rightPressed=true;
+   }
+ else  if(e.key==="Left" || e.key==="ArrowLeft")
+   {
+    leftPressed=true;
+   }
+}
+function keyUpHandler(e)
+{
+   if(e.key==="Right" || e.key==="ArrowRight")
+   {
+    rightPressed=false;
+   }
+ else  if(e.key==="Left" || e.key==="ArrowLeft")
+   {
+    leftPressed=false;
+   }
+}
 
 function drawBall(){
    
@@ -18,9 +55,19 @@ function drawBall(){
     ctx.fill();
     ctx.closePath();
 }
+function drawPaddle(){
+   
+    ctx.beginPath();
+    ctx.rect(paddleX,canvas.height-paddleHeight,paddleWidth,paddleHeight);
+    ctx.fillStyle="#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     // checking the collision with the wall . if ball collid with wall we change the diection
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -30,7 +77,15 @@ function draw() {
         dy = -dy;
       
     }
-  
+  // Adding paddle moving logic
+    if(rightPressed)
+    {
+        paddleX=Math.min(paddleX+7,canvas.width-paddleWidth);
+    }
+    if(leftPressed)
+    {
+        paddleX=Math.max(paddleX-7,0);
+    }
     x += dx;
     y += dy;
    
